d28284cf1ac5cb6253b592a28719bf93e3445c93
Carlos Nicolas <v-carlos.nicolas@aceable.com>
2022-06-20 13:13:24 -0500

    Logger v0.1

--- README.md
+++ README.md
@@ -13,8 +13,14 @@ Follow the next steps to run this app.
 - Clone the current repository
 - Go to the directory
 - Install using `composer install`
+- Duplicate the .env.example file and rename to `.env` only
+- Set the environment values "GEO_CLIENT" and "GEO_KEY" using a valid GeoIP2 keys.
+- Run server with this command `php -S localhost:8000 -t public`
+- Access to the url `localhost:8000`or running the cli command `php public/index.php`
+- The log file is in the `public/log.svg` directory
 
 
-## License
+## Additional packages
 
-The Lumen framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+* MobileDetect - [Website] (http://mobiledetect.net/)
+* GeoIP2 - [Website] (https://dev.maxmind.com/geoip/geolocate-an-ip/web-services)
--- app/Http/Controllers/LoggerController.php
+++ app/Http/Controllers/LoggerController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use \GeoIp2\WebService\Client;
+use Detection\MobileDetect;
+
 class LoggerController extends Controller
 {
     private $client;
@@ -18,44 +21,106 @@ class LoggerController extends Controller
     }
 
     public function index() {
-        $this->log("Client access.");
+        $this->log("Client access");
     }
 
-    public function geolocate() {
-        $data["country"] = null;
-        $data["city"] = null;
-        $data["ip"] = null;
-        
-        // Get IP
+    public function getClientIP()
+    {
+        $m = null;
+        try {
+            $externalContent = @file_get_contents('http://checkip.dyndns.com/');
+            preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return $m[1] ?? null;
+    }
 
-        // connect to API
+    public function getBrowserData()
+    {
+        $u_agent = $_SERVER['HTTP_USER_AGENT'] ?? null;
+        $name = 'Unknown';
+        $platform = 'Unknown';
+        $ub = "Unknown";
+        if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) {
+            $name = 'Internet Explorer';
+            $ub = "MSIE";
+        }
+        elseif(preg_match('/Firefox/i',$u_agent)) {
+            $name = 'Mozilla Firefox';
+            $ub = "Firefox";
+        }
+        elseif(preg_match('/Chrome/i',$u_agent)) {
+            $name = 'Google Chrome';
+            $ub = "Chrome";
+        }
+        elseif(preg_match('/Safari/i',$u_agent)) {
+            $name = 'Apple Safari';
+            $ub = "Safari";
+        }
+        elseif(preg_match('/Opera/i',$u_agent)) {
+            $name = 'Opera';
+            $ub = "Opera";
+        }
+        elseif(preg_match('/Netscape/i',$u_agent)) {
+            $name = 'Netscape';
+            $ub = "Netscape";
+        }
+        return (object) [ "name" => $name, "ub" => $ub ];
+    }
 
-        return $data;
+    public function getDeviceType()
+    {
+        $detect = new MobileDetect;
+        if ( $detect->isMobile() )
+            return "Mobile";
+        if( $detect->isTablet() )
+            return "Tablet";
+        return "Desktop";
     }
 
     public function getClient() {
-        $data["device"] = null;
-        $data["browser"] = null;
+        return (object) [
+            "ip" => $this->getClientIP(),
+            "device" => $this->getDeviceType(),
+            "browser" => $this->getBrowserData()
+        ];
+    }
 
-        // get client data
-
+    public function geolocate() {
+        $data = (object) [
+            "country" => "Unknown",
+            "state" => "Unknown"
+        ];
+        
+        // Geolocation
+        if($this->client->ip){
+            $client = new Client(env('GEO_CLIENT', ""), env('GEO_KEY', ""),['en'],['host' => 'geolite.info']);
+            $record = $client->city($this->client->ip);
+    
+            $data->country = $record->country->name;
+            $data->state = $record->subdivisions[0]->name;
+        }
         return $data;
     }
 
     public function store($action = "") {
         try {
-      
+            $handle = fopen(public_path("log.csv"), "a");
+            echo "Action: ".$action; 
+            fputcsv($handle, [$action]);
+            fclose($handle);
+            exit;
         } catch(Exception $e) {
-          echo "Cannot access to log file. ".$e->textError();
+          echo "Cannot access to log file. ".$e->getMessage(); exit;
         }
     }
 
     public function log($action) {
-        ["device" => $device, "browser" => $browser] = $this->getClient();
-        ["ip" => $ip, "country" => $country, "city" => $city] = $this->geolocate();
-        $dateTime = date('m/d/Y h:i:s a', time());
-        $log = "${dateTime} ${ip} ${country} ${city} - ${device} ${browser} ${action}";
+        $dateTime = date('m/d/Y H:i:s', time());
+        $log = "{$dateTime} - {$this->client->ip} - {$action} [{$this->geolocation->country} {$this->geolocation->state} - {$this->client->device} / {$this->client->browser->ub} / {$this->client->browser->name}]";
+        $this->store($log);
         echo $log;
-        //$this->store($log);
     }
 }
